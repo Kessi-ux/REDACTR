3e3c6b3bb4edd6657dffcf6d763c1a5d771c84ef
Kesandu <chukwubueze.uchenyi.231361@unn.edu.ng>
2023-11-05 17:42:41 +0100

    print the sentence in the DOM and put it into an  array of words

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
+// get the form data from the html file
+
 const scrambleData = document.querySelector('#scrambleForm');
+const output = document.querySelector('#outputData')
 
 scrambleData.addEventListener('submit', function(e)
     {
     e.preventDefault();
+
     const textInput = (scrambleData.elements.text);
     const wordInput = (scrambleData.elements.word);
 
     console.log(textInput.value);
     console.log(wordInput.value);
 
-    // addData (textInput.value)
-    // textInput.value = "";
-    });
+    // Get the input value
+    const sentence = textInput.value
 
+    // Split the sentence into an array of words
+    const wordsArray = sentence.split(" ");
+
+    // Display the words array in the console (for demonstration purposes)
+    console.log(wordsArray);
+
+    // You can do further processing with the wordsArray here
+    
+    // If you want to display the words in an HTML element (for demonstration purposes)
+        // addData(wordsArray);
+
+    addData (textInput.value, wordInput.value)
+    textInput.value= "";
+    wordInput.value= "";
+
+});
+
+const addData = (textInput, wordInput, words) => {
+    
+    const post = document.createElement('div');
+
+    const ptag = document.createElement('p');
+
+    ptag.append(`${textInput}`);
+
+    post.append(ptag);
+
+    output.append(post)
+    
+    // function to display the words to the DOMgit commit
+    // const outputDiv = document.createElement('div');
+    //     outputDiv.textContent = "Words in the sentence: " + words.join(", ");
+    //     document.body.appendChild(outputDiv);
+        
+    };
+
+    
